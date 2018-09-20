324cd8905e8d26293013fa38a569eaf81089f84a
Abd Rahim <mohamad.aliff.abd.rahim@intel.com>
2018-09-20 15:05:10 +0800

    Visual Studio 2015
    Adding visual Studio 2017
    Signed-off-by: Abd Rahim <mohamad.aliff.abd.rahim@intel.com>

---
+++ Visual Studio 2015/Projects/try/try/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace @try
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Adip = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Adip
+            // 
+            this.button_Adip.Location = new System.Drawing.Point(191, 110);
+            this.button_Adip.Name = "button_Adip";
+            this.button_Adip.Size = new System.Drawing.Size(118, 39);
+            this.button_Adip.TabIndex = 0;
+            this.button_Adip.Text = "Adip";
+            this.button_Adip.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 381);
+            this.Controls.Add(this.button_Adip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Adip;
+    }
+}
+
---
+++ Visual Studio 2015/Projects/try/try/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @try
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Form1 form = new Form1();
+            //form.Load += Form_Load;
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("im loaded");
+            
+        }
+
+        private void Button_Adip_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("im clicked");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button_Adip.Click += Button_Adip_Click;
+        }
+    }
+}
